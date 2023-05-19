238fd5f76dd01bffedcbc2ce882e1633baba3d05
Miguel-Luis-1 <migueluis1701@gmail.com>
2023-05-19 19:49:18 -0300

    .

--- lib/widgets/conta.dart
+++ lib/widgets/conta.dart
@@ -7,7 +7,6 @@ Widget build(BuildContext context) {
 
   return Container(
 
-
     decoration: BoxDecoration(
       color: Colors.white,
       border: Border.all(
@@ -19,17 +18,25 @@ Widget build(BuildContext context) {
 
     margin: const EdgeInsets.symmetric(vertical: 4),
     padding: const EdgeInsets.all(10),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    child: Row(
       children: [
-
-        Text('Conta de Luz'),
-        Text('12/01/2029'),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
         
+          children: [
+            
+            Text('Conta de Luz'),
+            Text(' | '),
+            Text('12/01/2029'),
+            Text(' | '),
 
+          ],
+          
+
+        ),
       ],
     ),
-
   );
 
 }
